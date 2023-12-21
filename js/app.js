67be74e2a6800448de43252a1f53a20dc876079f
VasilyProfWork <80577935+VasilyProfWork@users.noreply.github.com>
2023-12-21 18:54:05 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -500,11 +500,14 @@
                 document.documentElement.classList.add(className);
             }));
         }
+        function getHash() {
+            if (location.hash) return location.hash.replace("#", "");
+        }
         let bodyLockStatus = true;
         let bodyLockToggle = (delay = 500) => {
-            if (document.documentElement.classList.contains("lock")) functions_bodyUnlock(delay); else bodyLock(delay);
+            if (document.documentElement.classList.contains("lock")) bodyUnlock(delay); else bodyLock(delay);
         };
-        let functions_bodyUnlock = (delay = 500) => {
+        let bodyUnlock = (delay = 500) => {
             let body = document.querySelector("body");
             if (bodyLockStatus) {
                 let lock_padding = document.querySelectorAll("[data-lp]");
@@ -546,7 +549,83 @@
                 }
             }));
         }
+        function menuClose() {
+            bodyUnlock();
+            document.documentElement.classList.remove("menu-open");
+        }
+        function functions_FLS(message) {
+            setTimeout((() => {
+                if (window.FLS) console.log(message);
+            }), 0);
+        }
+        let gotoblock_gotoBlock = (targetBlock, noHeader = false, speed = 500, offsetTop = 0) => {
+            const targetBlockElement = document.querySelector(targetBlock);
+            if (targetBlockElement) {
+                let headerItem = "";
+                let headerItemHeight = 0;
+                if (noHeader) {
+                    headerItem = "header.header";
+                    headerItemHeight = document.querySelector(headerItem).offsetHeight;
+                }
+                let options = {
+                    speedAsDuration: true,
+                    speed,
+                    header: headerItem,
+                    offset: offsetTop,
+                    easing: "easeOutQuad"
+                };
+                document.documentElement.classList.contains("menu-open") ? menuClose() : null;
+                if ("undefined" !== typeof SmoothScroll) (new SmoothScroll).animateScroll(targetBlockElement, "", options); else {
+                    let targetBlockElementPosition = targetBlockElement.getBoundingClientRect().top + scrollY;
+                    targetBlockElementPosition = headerItemHeight ? targetBlockElementPosition - headerItemHeight : targetBlockElementPosition;
+                    targetBlockElementPosition = offsetTop ? targetBlockElementPosition - offsetTop : targetBlockElementPosition;
+                    window.scrollTo({
+                        top: targetBlockElementPosition,
+                        behavior: "smooth"
+                    });
+                }
+                functions_FLS(`[gotoBlock]: Юхуу...едем к ${targetBlock}`);
+            } else functions_FLS(`[gotoBlock]: Ой ой..Такого блока нет на странице: ${targetBlock}`);
+        };
         let addWindowScrollEvent = false;
+        function pageNavigation() {
+            document.addEventListener("click", pageNavigationAction);
+            document.addEventListener("watcherCallback", pageNavigationAction);
+            function pageNavigationAction(e) {
+                if ("click" === e.type) {
+                    const targetElement = e.target;
+                    if (targetElement.closest("[data-goto]")) {
+                        const gotoLink = targetElement.closest("[data-goto]");
+                        const gotoLinkSelector = gotoLink.dataset.goto ? gotoLink.dataset.goto : "";
+                        const noHeader = gotoLink.hasAttribute("data-goto-header") ? true : false;
+                        const gotoSpeed = gotoLink.dataset.gotoSpeed ? gotoLink.dataset.gotoSpeed : 500;
+                        const offsetTop = gotoLink.dataset.gotoTop ? parseInt(gotoLink.dataset.gotoTop) : 0;
+                        gotoblock_gotoBlock(gotoLinkSelector, noHeader, gotoSpeed, offsetTop);
+                        e.preventDefault();
+                    }
+                } else if ("watcherCallback" === e.type && e.detail) {
+                    const entry = e.detail.entry;
+                    const targetElement = entry.target;
+                    if ("navigator" === targetElement.dataset.watch) {
+                        document.querySelector(`[data-goto]._navigator-active`);
+                        let navigatorCurrentItem;
+                        if (targetElement.id && document.querySelector(`[data-goto="#${targetElement.id}"]`)) navigatorCurrentItem = document.querySelector(`[data-goto="#${targetElement.id}"]`); else if (targetElement.classList.length) for (let index = 0; index < targetElement.classList.length; index++) {
+                            const element = targetElement.classList[index];
+                            if (document.querySelector(`[data-goto=".${element}"]`)) {
+                                navigatorCurrentItem = document.querySelector(`[data-goto=".${element}"]`);
+                                break;
+                            }
+                        }
+                        if (entry.isIntersecting) navigatorCurrentItem ? navigatorCurrentItem.classList.add("_navigator-active") : null; else navigatorCurrentItem ? navigatorCurrentItem.classList.remove("_navigator-active") : null;
+                    }
+                }
+            }
+            if (getHash()) {
+                let goToHash;
+                if (document.querySelector(`#${getHash()}`)) goToHash = `#${getHash()}`; else if (document.querySelector(`.${getHash()}`)) goToHash = `.${getHash()}`;
+                goToHash ? gotoblock_gotoBlock(goToHash, true, 500, 20) : null;
+            }
+        }
         setTimeout((() => {
             if (addWindowScrollEvent) {
                 let windowScroll = new Event("windowScroll");
@@ -3394,12 +3473,6 @@
             };
         }();
         __webpack_require__(90);
-        document.addEventListener("click", (function(event) {
-            if (!event.target.closest(".menu") && !event.target.closest(".icon-menu")) {
-                document.documentElement.classList.remove("menu-open");
-                bodyUnlock();
-            }
-        }));
         document.addEventListener("DOMContentLoaded", (function() {
             function initSliders() {
                 if (document.querySelector("#splide1")) new Splide("#splide1", {
@@ -3474,8 +3547,52 @@
             }));
         }
         mascTel();
+        function myModal() {
+            let modalButtons = document.querySelectorAll(".js-open-modal");
+            const lock = document.querySelector("html");
+            let overlay = document.querySelector(".js-overlay-modal");
+            let closeButtons = document.querySelectorAll(".js-modal-close");
+            document.addEventListener("click", (function(event) {
+                if (!event.target.closest(".menu") && !event.target.closest(".icon-menu")) {
+                    document.documentElement.classList.remove("menu-open");
+                    lock.classList.remove("lock");
+                }
+                if (overlay.classList.contains("active")) lock.classList.add("lock");
+            }));
+            for (let elemBtn of modalButtons) elemBtn.addEventListener("click", (function(e) {
+                e.preventDefault();
+                let modalId = this.getAttribute("data-modal");
+                let modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
+                if (!modalElem.classList.contains("active")) modalElem.classList.add("active");
+                overlay.classList.add("active");
+                lock.classList.add("lock");
+                let parentBody = this.closest(".modal");
+                if (parentBody.classList.contains("active")) parentBody.classList.remove("active");
+            }));
+            for (let elem of closeButtons) elem.addEventListener("click", (function() {
+                var parentModal = this.closest(".modal");
+                parentModal.classList.remove("active");
+                overlay.classList.remove("active");
+                lock.classList.remove("lock");
+            }));
+            document.body.addEventListener("keyup", (function(e) {
+                var key = e.keyCode;
+                if (27 == key) {
+                    document.querySelector(".modal.active").classList.remove("active");
+                    document.querySelector(".overlay").classList.remove("active");
+                    lock.classList.remove("lock");
+                }
+            }), false);
+            overlay.addEventListener("click", (function() {
+                document.querySelector(".modal.active").classList.remove("active");
+                this.classList.remove("active");
+                lock.classList.remove("lock");
+            }));
+        }
+        myModal();
         window["FLS"] = true;
         isWebp();
         menuInit();
+        pageNavigation();
     })();
 })();
